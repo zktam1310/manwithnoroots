ad6945e08171a54e3e9672ce6ed3b7d59abb48db
Zhi Kang Tam <zktam@Zks-Mac.local>
2021-12-29 08:25:54 +0800

    Resolving Tailwind media query issues

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': {"max": '480px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   variants: {
